1d446753a55615ba64bff4d59e57e0128e6fb681
0m3ga13 <barkaoussama13@gmail.com>
2023-08-05 18:19:22 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+const findMatching = (array, str) => array.filter(item => item.toUpperCase() == str.toUpperCase())
+
+const fuzzyMatch = (array,str) => array.filter(item => item.startsWith(str))
+
+const matchName = (array,str) => array.filter(item => item.name == str)
